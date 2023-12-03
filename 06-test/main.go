5e0b8cb1ad58bf44769ab14d91413ee1687a07b1
zhuhuoshi <zhuhuoshi@im30.lan>
2023-12-03 21:08:33 +0800

    feat: 新增const 和iota

--- 06-test/main.go
+++ 06-test/main.go
@@ -5,18 +5,31 @@ import (
 	"time"
 )
 
-func main() {
-	ch := make(chan int, 0)
-	fmt.Println(time.Now().Unix())
-	go func() {
-		a := <-ch
-		fmt.Printf("%d 读出%d\n", time.Now().Unix(), a)
-	}()
-	time.Sleep(2 * time.Second)
-	go func() {
-		ch <- 10
-		fmt.Println(time.Now().Unix(), "写入10成功")
-	}()
+const (
+	A = 1 + iota
+	B
+	C
+	_
+	E = iota
+)
 
-	time.Sleep(1 * time.Second)
+func main() {
+	fmt.Println(time.Now().Unix())
+	fmt.Printf("B的值为：%d C的值为：%d E的值为：%d\n", B, C, _)
 }
+
+// func main() {
+// 	ch := make(chan int, 0)
+// 	fmt.Println(time.Now().Unix())
+// 	go func() {
+// 		a := <-ch
+// 		fmt.Printf("%d 读出%d\n", time.Now().Unix(), a)
+// 	}()
+// 	time.Sleep(2 * time.Second)
+// 	go func() {
+// 		ch <- 10
+// 		fmt.Println(time.Now().Unix(), "写入10成功")
+// 	}()
+
+// 	time.Sleep(1 * time.Second)
+// }
